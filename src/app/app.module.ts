36a485ce5422d21100767dbeffcf686348d6a108
William Lees <william@lees.org.uk>
2019-12-04 10:54:13 +0000

    Initial menu structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { SubmittedAllelesComponent } from './submitted-alleles/submitted-alleles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SubmittedAllelesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/submitted-alleles/submitted-alleles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submitted-alleles',
+  templateUrl: './submitted-alleles.component.html',
+  styleUrls: ['./submitted-alleles.component.sass']
+})
+export class SubmittedAllelesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

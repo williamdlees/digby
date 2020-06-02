d0eec54f7c9274a1f03aae1b86f52b1aaff2fbf8
William Lees <william@lees.org.uk>
2020-06-02 15:56:51 +0100

    New gene table

---
+++ src/app/rep-gene-table/rep-gene-table-panel/rep-gene-table-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rep-gene-table-panel',
+  templateUrl: './rep-gene-table-panel.component.html',
+  styleUrls: ['./rep-gene-table-panel.component.scss']
+})
+export class RepGeneTablePanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/rep-gene-table/rep-gene-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rep-gene-table',
+  templateUrl: './rep-gene-table.component.html',
+  styleUrls: ['./rep-gene-table.component.scss']
+})
+export class RepGeneTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

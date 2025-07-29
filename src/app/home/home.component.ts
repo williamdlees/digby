645b3d3dc7bdc4d619df16b1e6dc7061f841a7bd
William Lees <william@lees.org.uk>
2025-07-29 14:05:48 +0100

    Improve header text and wrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,7 +11,7 @@ import { RouterLink } from '@angular/router';
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
-    imports: [RouterLink]
+    standalone: true
 })
 export class HomeComponent implements OnInit {
   loadedNewsPosts = [];
